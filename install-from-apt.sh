4cac2208c8e2010d4cac034de7100cc435e6214c
Karol <egzepl@gmail.com>
2022-03-03 17:23:17 +0100

    Update install-from-apt.sh

--- install-from-apt.sh
+++ install-from-apt.sh
@@ -1,1 +1,4 @@
 #!bin/bash
+sudo apt install -y adb calibre chromium-browser code curl docker-ce docker-ce-cli \
+gcc git gnome-shell-extensions gnome-tweaks htop make musescore3 neofetch openjdk-17-jdk \
+tabby-terminal teams tmux virtualbox zsh
